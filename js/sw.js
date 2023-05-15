412906f5a3d0f2ccfbdb11b491bea0306c03afee
Garmayev <garmayev@yandex.ru>
2023-05-15 17:49:06 +0800

    Simplify

---
+++ js/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", (e) => {
+    console.log(e);
+});
+
+self.addEventListener("activate", (e) => {
+    console.log(e);
+});
--- src/components/other/Other.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class Other extends React.Component {
-    render() {
-        return (
-            <div>Other</div>
-        );
-    }
-}
